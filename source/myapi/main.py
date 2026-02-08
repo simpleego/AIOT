15e65d08a212fa7041d79bef4806f72d02b0af7d
Jong Cheon Park <simpleego@gmail.com>
2026-02-09 07:43:41 +0900

    Add files via upload

---
+++ source/myapi/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from domain.question import question_router
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",    # 또는 "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요 파이보"}
+
+app.include_router(question_router.router)
